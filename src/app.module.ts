d1942e2e24d2a0160edadfc7404a84fed41c7e53
botirjon13 <tomama-uz@mail.ru>
2026-06-05 14:29:56 +0500

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
+import configuration from './config/configuration';
+import { validate } from './config/env.validation';
+import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validate,
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    HealthModule,
+  ],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
