17333b7835e8bcd0977bebcdaea1f142af085743
botirjon13 <tomama-uz@mail.ru>
2026-06-10 20:29:27 +0500

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,8 +23,8 @@ export class UsersService {
         id: true,
         email: true,
         role: true,
-        created_at: true,
-        updated_at: true,
+        createdAt: true,
+        updatedAt: true,
       },
     });
 
