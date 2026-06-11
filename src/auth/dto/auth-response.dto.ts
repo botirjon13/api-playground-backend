1015289fdf3020c8129de68a41c238c97f29214e
botirjon13 <tomama-uz@mail.ru>
2026-06-11 09:12:15 +0500

    Update auth-response.dto.ts

--- src/auth/dto/auth-response.dto.ts
+++ src/auth/dto/auth-response.dto.ts
@@ -1,14 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterResponseDto {
-  @ApiProperty({ example: '2ef32d9d-3b82-4f52-a5dd-abc7c3f4fd28' })
-  id!: string;
-
-  @ApiProperty({ example: 'user@example.com' })
-  email!: string;
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
+  access_token!: string;
 }
 
 export class LoginResponseDto {
-  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
   access_token!: string;
 }
