fc58155978e1fe8505e98d94e4243b0c3843fa34
botirjon13 <tomama-uz@mail.ru>
2026-06-11 09:04:48 +0500

    Update register.dto.ts

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,13 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
-  @ApiProperty({ example: 'user@example.com', description: 'Unique account email address.' })
-  @IsEmail({}, { message: 'email must be a valid email address' })
+  @ApiProperty({
+    example: 'Ilhomjon Ergashev',
+  })
+  @IsString()
+  @MinLength(2)
+  fullName!: string;
+
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Unique account email address.',
+  })
+  @IsEmail({}, {
+    message: 'email must be a valid email address',
+  })
   email!: string;
 
-  @ApiProperty({ example: 'Password123', minLength: 8, description: 'Account password.' })
-  @IsString({ message: 'password must be a string' })
-  @MinLength(8, { message: 'password must be at least 8 characters long' })
+  @ApiProperty({
+    example: 'Password123',
+    minLength: 8,
+    description: 'Account password.',
+  })
+  @IsString({
+    message: 'password must be a string',
+  })
+  @MinLength(8, {
+    message: 'password must be at least 8 characters long',
+  })
   password!: string;
 }
